34b5f69b0641e5d499c2d39c0c966852a6bd45e0
ameer <ameer.com>
2020-01-27 14:04:06 +0530

    Added calc function

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x,y)
+    """Add Function"""
+    return x + y
+
+def subtract(x,y)
+    """Subtract Function"""
+    pass
+
+def multiply(x,y)
+    """Mutiply Function"""
+    return x * y
+
+def divide(x,y)
+    """Divide Function"""
+    return x / y
